d92ad3b9a76bc24259dbbf387172c630fe2b1912
Yaniv Vyssokii <yanivv@campus.technion.ac.il>
2022-04-26 17:46:17 +0300

    Added RLEListSize, RLEListGet, RLEListMap functions

--- RLEList.c
+++ RLEList.c
@@ -1,7 +1,44 @@
 #include "RLEList.h"
 
 struct RLEList_t{
-    //TODO: implement
-};
+    int amount;
+    char letter;
+    struct RLEList_t* next;
+}
 
-//implement the functions here
+int RLEListSize(RLEList list){
+    if (!list){
+        return -1;
+    }
+    int size = 0;
+    while (list){
+        size+=list->amount;
+        list=list->next;
+    }
+    return size;
+}
+
+char RLEListGet(RLEList list, int index, RLEListResult *result){
+    if (result!=RLE_LIST_SUCCESS){
+        return 0;
+    }
+    if (index<0||index>=RLEListSize(list)||){
+        return RLE_LIST_ERROR;
+    }
+    while (index-list->amount>0){
+        index-=list->amount;
+        list=list->next;
+    }
+    return list->letter;
+}
+
+RLEListResult RLEListMap(RLEList list, MapFunction map_function){
+    if (!list||!map_function){
+        return RLE_LIST_NULL_ARGUMENT;
+    }
+    while(list){
+        list->letter=(*map_function)(list->letter);
+        list=list->next;
+    }
+    return LIST_SUCCESS;
+}
