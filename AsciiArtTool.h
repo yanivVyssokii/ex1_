2da0d74d930365b9806d497896fadec046c3bb46
Yaniv Vyssokii <yanivv@campus.technion.ac.il>
2022-04-27 13:14:25 +0300

    Done commenting the needed functions

--- AsciiArtTool.h
+++ AsciiArtTool.h
@@ -9,12 +9,31 @@
 
 /**
  * asciiArtRead: Returns an RLEList of the text in the file
- *
+ * @param in_stream - the file object of the input
+ * @return
+ *  The RLE list that represents the given file
  */
 RLEList asciiArtRead(FILE* in_stream);
 
+/**
+ * asciiArtPrint: Prints the image represented by the given RLE list to the given file
+ * @param list - the RLE list which represents the wanted image
+ * @param out_stream - the file object the write the image into
+ * @return
+ *  RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function.
+ *  RLE_LIST_SUCCESS if the list was printed printed to the file successfully
+ */
 RLEListResult asciiArtPrint(RLEList list, FILE *out_stream);
 
+
+/**
+ * asciiArtPrintEncoded: Prints the given RLE list to the given file
+ * @param list - the RLE list which represents the wanted image
+ * @param out_stream - the file object the write the image into
+ * @return
+ *  RLE_LIST_NULL_ARGUMENT if a NULL was sent to the function.
+ *  RLE_LIST_SUCCESS if the list was printed printed to the file successfully
+ */
 RLEListResult asciiArtPrintEncoded(RLEList list, FILE *out_stream);
 
 
